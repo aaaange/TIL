6252ee64ae1a195b9e94236fc50691c8b44c4a34
aaaange <ayung00@naver.com>
2024-12-12 20:42:17 +0900

    ONEBITE-REACT section05-02 Components

---
+++ React/ONEBITE-REACT/section05/section05/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer>
+            <h1>This is footer Component!</h1>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ React/ONEBITE-REACT/section05/section05/src/components/Main.jsx
@@ -0,0 +1,9 @@
+function Main () {
+    return (
+        <main>
+            <h1>this is main Component!</h1>
+        </main>
+    );
+}
+
+export default Main;
