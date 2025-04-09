b85d76653a890dfd9d554a506e41e2ee53ce7edf
aaange <ayung00@naver.com>
2025-04-09 16:47:10 +0900

    feat:(section02) 04강 주석(Comment)

---
+++ Java/01_intro_java/java-start/src/CommentJava.java
@@ -0,0 +1,11 @@
+public class CommentJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java1"); // hello java1을 출력합니다.
+        // System.out.println("Hello Java2"); // 한 줄 주석
+
+        /* 여러 줄 주석
+        System.out.println("Hello Java1"); // hello java1을 출력합니다.
+        System.out.println("Hello Java1"); // hello java1을 출력합니다.
+         */
+    }
+}
